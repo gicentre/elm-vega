0abb98b95857974c624d3454047cc2ff6dabcbe4
Jo Wood <jwo@soi.city.ac.uk>
2018-04-25 09:53:31 +0100

    Hello world test

---
+++ vTests/src/HelloWorld.elm
@@ -0,0 +1,69 @@
+port module HelloWorld exposing (elmToJS)
+
+import Platform
+import Vega exposing (..)
+
+
+helloWorld : Spec
+helloWorld =
+    let
+        table =
+            dataFromColumns "table" []
+                << dataColumn "label" (daStrs [ "Hello", "from", "elm-vega" ])
+                << dataColumn "x" (daNums [ 1, 2, 3 ])
+
+        ds =
+            dataSource [ table [] ]
+
+        sc =
+            scales
+                << scale "xscale"
+                    [ scDomain (doData [ dDataset "table", dField (str "x") ])
+                    , scRange (raDefault RWidth)
+                    ]
+
+        mk =
+            marks
+                << mark Text
+                    [ mFrom [ srData (str "table") ]
+                    , mEncode
+                        [ enEnter
+                            [ maX [ vScale (fName "xscale"), vField (fName "x") ]
+                            , maText [ vField (fName "label") ]
+                            ]
+                        ]
+                    ]
+    in
+    toVega
+        [ width 100, ds, sc [], mk [] ]
+
+
+
+{- This list comprises the specifications to be provided to the Vega runtime.
+   In this example, only a single spec 'helloWord' is provided.
+-}
+
+
+mySpecs : Spec
+mySpecs =
+    combineSpecs [ ( "helloWorld", helloWorld ) ]
+
+
+
+{- ---------------------------------------------------------------------------
+   The code below is boilerplate for creating a headless Elm module that opens
+   an outgoing port to JavaScript and sends the Vega specs (mySpecs) to it.
+   There should be no need to change this.
+-}
+
+
+main : Program Never Spec msg
+main =
+    Platform.program
+        { init = ( mySpecs, elmToJS mySpecs )
+        , update = \_ model -> ( model, Cmd.none )
+        , subscriptions = always Sub.none
+        }
+
+
+port elmToJS : Spec -> Cmd msg
