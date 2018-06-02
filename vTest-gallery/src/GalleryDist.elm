c586d68862cbb618302fca4f94b5b1816feb5623
Jo Wood <jwo@soi.city.ac.uk>
2018-06-02 07:31:06 +0100

    Null histogram example

--- vTest-gallery/src/GalleryDist.elm
+++ vTest-gallery/src/GalleryDist.elm
@@ -19,23 +19,13 @@ histo1 =
             dataSource
                 [ data "points" [ daUrl "https://vega.github.io/vega/data/normal-2d.json" ]
                 , data "binned" [ daSource "points" ]
-                    -- |> transform
-                    --     [ trBin (str "u")
-                    --         (num -1)
-                    --         (num 1)
-                    --         [ bnAnchor (numSignal "binOffset")
-                    --         , bnStep (numSignal "binStep")
-                    --         , bnNice (boo False)
-                    --         ]
-                    --     , trAggregate
-                    --         [ agKey (str "bin0")
-                    --         , agGroupBy [ str "bin0", str "bin1" ]
-                    --         , agOps [ Count ]
-                    --         , agAs [ "count" ]
-                    --         ]
-                    --     ]
                     |> transform
-                        [ trBin (str "u") (num -1) (num 1) []
+                        [ trBin (str "u")
+                            (nums [ -1, 1 ])
+                            [ bnAnchor (numSignal "binOffset")
+                            , bnStep (numSignal "binStep")
+                            , bnNice (boo False)
+                            ]
                         , trAggregate
                             [ agKey (str "bin0")
                             , agGroupBy [ str "bin0", str "bin1" ]
@@ -110,9 +100,110 @@ histo1 =
         [ width 500, height 100, padding 5, ds, si [], sc [], ax [], mk [] ]
 
 
+histo2 : Spec
+histo2 =
+    let
+        ds =
+            dataSource
+                [ data "table" [ daUrl "https://vega.github.io/vega/data/movies.json" ]
+                    |> transform
+                        [ trExtentAsSignal (str "IMDB_Rating") "extent"
+                        , trBin (str "IMDB_Rating")
+                            (numSignal "extent")
+                            [ bnSignal "bins"
+                            , bnMaxBins (numSignal "maxBins")
+                            ]
+                        ]
+                , data "counts" [ daSource "table" ]
+                    |> transform
+                        [ trFilter (expr "datum['IMDB_Rating'] != null")
+                        , trAggregate [ agGroupBy [ str "bin0", str "bin1" ] ]
+                        ]
+                , data "nulls" [ daSource "table" ]
+                    |> transform
+                        [ trFilter (expr "datum['IMDB_Rating'] == null")
+                        , trAggregate []
+                        ]
+                ]
+
+        si =
+            signals
+                << signal "maxBins" [ siValue (vNum 10), siBind (iSelect [ inOptions (vNums [ 5, 10, 20 ]) ]) ]
+                << signal "binDomain" [ siUpdate "sequence(bins.start, bins.stop + bins.step, bins.step)" ]
+                << signal "nullGap" [ siValue (vNum 10) ]
+                << signal "barStep" [ siUpdate "(width - nullGap) / binDomain.length" ]
+
+        sc =
+            scales
+                << scale "xScale"
+                    [ scType ScBinLinear
+                    , scRange (raValues [ vSignal "barStep + nullGap", vSignal "width" ])
+                    , scDomain (doNums (numSignal "binDomain"))
+                    , scRound (boo True)
+                    ]
+                << scale "xScaleNull"
+                    [ scType ScBand
+                    , scRange (raValues [ vNum 0, vSignal "barStep" ])
+                    , scRound (boo True)
+                    , scDomain (doStrs (strs [ "null" ]))
+                    ]
+                << scale "yScale"
+                    [ scType ScLinear
+                    , scRange (raDefault RHeight)
+                    , scRound (boo True)
+                    , scNice NTrue
+                    , scDomain
+                        (doData
+                            [ daReferences
+                                [ [ daDataset "counts", daField (str "count") ]
+                                , [ daDataset "nulls", daField (str "count") ]
+                                ]
+                            ]
+                        )
+                    ]
+
+        ax =
+            axes
+                << axis "xScale" SBottom [ axTickCount 10 ]
+                << axis "xScaleNull" SBottom []
+                << axis "yScale" SLeft [ axTickCount 5, axOffset (num 5) ]
+
+        mk =
+            marks
+                << mark Rect
+                    [ mFrom [ srData (str "counts") ]
+                    , mEncode
+                        [ enUpdate
+                            [ maX [ vScale (fName "xScale"), vField (fName "bin0"), vOffset (vNum 1) ]
+                            , maX2 [ vScale (fName "xScale"), vField (fName "bin1") ]
+                            , maY [ vScale (fName "yScale"), vField (fName "count") ]
+                            , maY2 [ vScale (fName "yScale"), vNum 0 ]
+                            , maFill [ vStr "steelblue" ]
+                            ]
+                        , enHover [ maFill [ vStr "firebrick" ] ]
+                        ]
+                    ]
+                << mark Rect
+                    [ mFrom [ srData (str "nulls") ]
+                    , mEncode
+                        [ enUpdate
+                            [ maX [ vScale (fName "xScaleNull"), vNull, vOffset (vNum 1) ]
+                            , maX2 [ vScale (fName "xScaleNull"), vBand 1 ]
+                            , maY [ vScale (fName "yScale"), vField (fName "count") ]
+                            , maY2 [ vScale (fName "yScale"), vNum 0 ]
+                            , maFill [ vStr "#aaa" ]
+                            ]
+                        , enHover [ maFill [ vStr "firebrick" ] ]
+                        ]
+                    ]
+    in
+    toVega
+        [ width 400, height 200, padding 5, autosize [ AFit, AResize ], ds, si [], sc [], ax [], mk [] ]
+
+
 sourceExample : Spec
 sourceExample =
-    histo1
+    histo2
 
 
 
@@ -123,6 +214,7 @@ mySpecs : Spec
 mySpecs =
     combineSpecs
         [ ( "histo1", histo1 )
+        , ( "histo2", histo2 )
         ]
 
 
