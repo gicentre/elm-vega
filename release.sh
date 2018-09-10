b7906fda44af89172138cf42aae00b5daee005d0
Jo Wood <jwo@soi.city.ac.uk>
2018-09-10 14:54:04 +0100

    Update publish script to use elm 0.19 binaries

--- release.sh
+++ release.sh
@@ -6,9 +6,9 @@ set -euxo pipefail
 
 rm -rf release || exit 0;
 
-elm-package bump
+elm package bump
 
-version=$(grep -m1 version elm-package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+version=$(grep -m1 version elm package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 git commit -a -m "Bump to $version"
 git push
@@ -24,5 +24,5 @@ git clone --reference . git@github.com:gicentre/elm-vega.git release
   git commit -m "Cleanup and release $version"
   git tag -a $version -m "Release $version"
   git push origin $version
-  elm-package publish
+  elm package publish
 )
